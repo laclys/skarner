8a73b9142833aca6fadba409854caadbd670df8d
Lac <laclys@126.com>
2020-07-18 23:52:04 +0800

    feat: add List

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,65 @@
 import React from 'react';
-import useSWR from 'swr'
-import fetch from 'unfetch'
-import Button from 'antd/es/button'
-import List from 'antd/es/list'
+import useSWR from 'swr';
+import fetch from 'unfetch';
+import Button from 'antd/es/button';
+import List from 'antd/es/list';
+import Avatar from 'antd/es/avatar';
 // css
-import 'antd/es/button/style/css'
-import 'antd/es/list/style/css'
+import 'antd/es/button/style/css';
+import 'antd/es/list/style/css';
+import 'antd/es/avatar/style/css'
 // css module
-import style from './App.module.css'
+import style from './App.module.css';
 
-const URL = `api/topics/hot.json`
+const URL = `api/topics/hot.json`;
 
 const getList = async () => {
-  const res = await fetch(URL)
-  return await res.json()
-}
+  const res = await fetch(URL);
+  return await res.json();
+};
 
 const App = () => {
-  const { data, error } = useSWR(URL, getList)
-  console.log('list', data)
-
-  if (error) {
-    return <div>failed to load</div>
-  }
+  const { data, error } = useSWR(URL, getList);
 
   return (
     <div className={style.wrapper}>
       <h3>Hot Themes</h3>
-      <Button className={style.reloadBtn}
+      <Button
+        className={style.reloadBtn}
         type="primary"
-        onClick={() => getList(URL)}>Reload</Button>
-      {(data && data.length !== 0 ) ? (
-        <div>
-          {data.length}
-        </div>
-      ) : <div>no data!</div>}
+        onClick={() => getList(URL)}
+      >
+        Reload
+      </Button>
+      <Content data={data} error={error} />
     </div>
   );
-}
+};
+
+const Content = ({ data, error }) => {
+  if (error) {
+    return <div>failed to load</div>;
+  }
+
+  return (
+    <div className={style.contentWrapper} >
+      <List
+      bordered
+      loading={!data}
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Avatar src={item?.member?.avatar_normal} />}
+            title={<a href={item?.url}>{item?.title}</a>}
+            description={item?.content}
+          />
+        </List.Item>
+      )}
+    />
+    </div>
+  );
+};
 
 export default App;
